4d0e4b39fdd0e97b77e9a68df5a2b9a75314ab56
Hiricus <hexaeder@yandex.ru>
2024-06-23 13:36:06 +0300

    ips and ports now always check the range

--- fw_core.py
+++ fw_core.py
@@ -8,24 +8,16 @@ class FirewallCore:
     # проверка на соответствие одного параметра из правила
     def checkRule(self, param, packet, rule_value):
         if param == 'direction':
-            check_value = str(int(packet.direction))  # напрямую в строку лучше не переводить
+            check_value = str(int(packet.direction))  # напрямую в строку лучше не переводить, через инт норм
         elif param == 'protocol':
             check_value = str(FWutils.protocols[packet.protocol[0]])
-        elif param == 'src_port':
-            check_value = str(packet.src_port)
-        elif param == 'dst_port':
-            check_value = str(packet.dst_port)
-        elif param == 'src_ip':
-            check_value = str(packet.src_addr)
-        elif param == 'dst_ip':
-            check_value = str(packet.dst_addr)
         else:
             raise ValueError(f'Invalid ip packet parameter: {param}')
 
         if rule_value == 'None':
-            self.__is_fit_F = self.__is_fit_F
+            pass
         elif rule_value == check_value:
-            self.__is_fit_F = self.__is_fit_F
+            pass
         else:
             self.__is_fit_F = False
 
@@ -46,9 +38,9 @@ class FirewallCore:
             check_value = -1
 
         if rule_values[0] == 'None':
-            self.__is_fit_F = self.__is_fit_F
+            pass
         elif (int(rule_values[0]) <= int(check_value)) and (int(check_value) <= int(rule_values[1])):
-            self.__is_fit_F = self.__is_fit_F
+            pass
         else:
             self.__is_fit_F = False
 
@@ -64,17 +56,17 @@ class FirewallCore:
 
         rule_values = rule_values.split('-')
         if rule_values[0] == 'None':
-            self.__is_fit_F = self.__is_fit_F
+            pass
         elif (FWutils.compareIpAddr(rule_values[0], check_value) in (0, 1)) and (FWutils.compareIpAddr(check_value, rule_values[1]) in (0, 1)):
-            self.__is_fit_F = self.__is_fit_F
+            pass
         else:
             self.__is_fit_F = False
 
+    # Типа синглтон
     def __new__(cls, *args, **kwargs):
         if cls.__instance is None:
             cls.__instance = super().__new__(cls)
         return cls.__instance
-
     def __del__(self):
         # self.__w.close()
         FirewallCore.__instance = None
@@ -95,6 +87,12 @@ class FirewallCore:
             self.rules = rulefile.readlines()
         self.rules = self.rules[1:]  # Убирает референсную строчку
 
+        # превращаем строку в список правил
+        for i in range(len(self.rules)):
+            self.rules[i] = self.rules[i].strip('\n')
+            self.rules[i] = self.rules[i].split(';')
+        # print(self.rules)
+
         # цикл работающий с пакетом
         for packet in self.__w:
             # print(FWutils.protocols[packet.protocol[0]])
@@ -107,37 +105,29 @@ class FirewallCore:
                 else:
                     pass
                 continue
+
             # цикл работающий с одним правилом
             for self.single_rule in self.rules:
                 self.__is_fit_F = True
-
-                # превращаем строку в список правил
-                self.single_rule = self.single_rule.strip('\n')
-                self.single_rule = self.single_rule.split(';')
-                # print(self.single_rule)
 
                 # проверки пакета
                 # i/o и протокол
                 self.checkRule('direction', packet, self.single_rule[1])
                 self.checkRule('protocol', packet, self.single_rule[2])
 
-                # порты
-                self.checkRule('src_port', packet, self.single_rule[3])
-                self.checkRangedRule_port('src_port_range', packet, self.single_rule[4])
-                self.checkRule('dst_port', packet, self.single_rule[5])
-                self.checkRangedRule_port('dst_port_range', packet, self.single_rule[6])
-
                 # адреса
-                self.checkRule('src_ip', packet, self.single_rule[7])
-                self.checkRangedRule_addr('src_ip_range', packet, self.single_rule[8])
-                self.checkRule('dst_ip', packet, self.single_rule[9])
-                self.checkRangedRule_addr('dst_ip_range', packet, self.single_rule[10])
+                self.checkRangedRule_addr('src_ip_range', packet, self.single_rule[3])
+                self.checkRangedRule_addr('dst_ip_range', packet, self.single_rule[4])
+
+                # порты
+                self.checkRangedRule_port('src_port_range', packet, self.single_rule[5])
+                self.checkRangedRule_port('dst_port_range', packet, self.single_rule[6])
 
                 # действие, если пакет подошёл под правило
                 if self.__is_fit_F:
-                    if self.single_rule[11] == 'allow':
+                    if self.single_rule[7] == 'allow':
                         self.__w.send(packet)
-                    elif self.single_rule[11] == 'reject':
+                    elif self.single_rule[7] == 'reject':
                         print(f'Rejected packet from ip {packet.src_addr}, on ports {packet.src_port}:{packet.dst_port}, protocol {FWutils.protocols[packet.protocol[0]]} using rule {self.single_rule[0]}     {str(packet.direction)}')
                         # print(self.single_rule)
                         print()
